f9fd44c7e1f005b77354c67534cbeb271db4d1fb
Helder rocha <hedie.strokes@gmail.com>
2017-06-29 20:29:19 -0300

    adicionado bloco sem ser string

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -9,14 +9,6 @@ angular.module('minhasDiretivas', [])
         };
 
         ddo.transclude = true; // pra manter os elemntos filhos
-        ddo.template  =
-                    '<div class="panel panel-default">'
-                        +'<div class="panel-heading">'
-                         +'  <h3 class="panel-title"> {{titulo}}</h3>'
-                        +'</div>'
-                        +'<div class="panel-body" ng-transclude>'
-                        +'</div>'
-                    +'</div>'
-
+        ddo.templateUrl = 'js/directives/meu-painel.html';
         return ddo;
     });
