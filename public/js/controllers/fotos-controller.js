ec5f8371233e10da87c7051f0b094bda33dc741f
Helder rocha <hedie.strokes@gmail.com>
2017-06-29 09:31:51 -0300

    sequencia de testes angular

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+//$scpe usado pra ter acesso ao scope do
+angular.module('alurapic').controller('FotosController', function ($scope) {
+    $scope. foto =  {
+        titulo : 'Dog Nerd',
+        url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+    }
+    
+});
---
+++ public/js/main.js
@@ -0,0 +1,2 @@
+//para criar é obrigadoa  passar a depedencia vazia
+angular.module('alurapic', []);
