3bc0d7640362bc212b03bfdc0fd0ab0e8b304eb9
Helder rocha <hedie.strokes@gmail.com>
2017-06-29 10:16:12 -0300

    integracao com backend criacao de promisse

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,37 +1,50 @@
-//$scpe usado pra ter acesso ao scope do
-angular.module('alurapic').controller('FotosController', function ($scope) {
-    $scope. fotos =  [
-        {
-            titulo : 'Dog Nerd 0',
-            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-        },
-        {
-            titulo : 'Dog Nerd 1',
-            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-        },
-        {
-            titulo : 'Dog Nerd 2',
-            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-        },
-        {
-            titulo : 'Dog Nerd 3',
-            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-        }
-    ]
+//$scpe usado pra ter acesso ao scope do, $http é o responsavel por fazer requisicao ajax
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+    $scope.fotos = [];
 
-    // $scope. foto1 =  {
-    //     titulo : 'Dog Nerd 1',
-    //     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-    // }
-    //
-    // $scope. foto2 =  {
-    //     titulo : 'Dog Nerd 2',
-    //     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-    // }
-    //
-    // $scope. foto3 =  {
-    //     titulo : 'Dog Nerd 3',
-    //     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-    // }
+    $http.get('v1/fotos')
+        .success(function (fotos) {
+            $scope.fotos = fotos;
+        })
+        .error(function (erro) {
+            console.log(erro);
+        });
 
+    // $http.get('v1/fotos');
+    // promise.then(function(retorno) {
+    //     $scope.fotos = retorno.data;
+    // }).catch(function (error) {
+    //     console.log(error);
+    // })
 });
+
+// {
+//     titulo : 'Dog Nerd 0',
+//         url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// },
+// {
+//     titulo : 'Dog Nerd 1',
+//         url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// },
+// {
+//     titulo : 'Dog Nerd 2',
+//         url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// },
+// {
+//     titulo : 'Dog Nerd 3',
+//         url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// }
+// $scope. foto1 =  {
+//     titulo : 'Dog Nerd 1',
+//     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// }
+//
+// $scope. foto2 =  {
+//     titulo : 'Dog Nerd 2',
+//     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// }
+//
+// $scope. foto3 =  {
+//     titulo : 'Dog Nerd 3',
+//     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+// }
