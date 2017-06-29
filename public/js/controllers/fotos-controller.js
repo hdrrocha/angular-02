bce9c4b3feed3757a0facb6379222643fc4cecbc
Helder rocha <hedie.strokes@gmail.com>
2017-06-29 09:59:44 -0300

    add ng-repeating

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,37 @@
 //$scpe usado pra ter acesso ao scope do
 angular.module('alurapic').controller('FotosController', function ($scope) {
-    $scope. foto =  {
-        titulo : 'Dog Nerd',
-        url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
-    }
-    
+    $scope. fotos =  [
+        {
+            titulo : 'Dog Nerd 0',
+            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+        },
+        {
+            titulo : 'Dog Nerd 1',
+            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+        },
+        {
+            titulo : 'Dog Nerd 2',
+            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+        },
+        {
+            titulo : 'Dog Nerd 3',
+            url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+        }
+    ]
+
+    // $scope. foto1 =  {
+    //     titulo : 'Dog Nerd 1',
+    //     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+    // }
+    //
+    // $scope. foto2 =  {
+    //     titulo : 'Dog Nerd 2',
+    //     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+    // }
+    //
+    // $scope. foto3 =  {
+    //     titulo : 'Dog Nerd 3',
+    //     url : 'http://www.ligadanasdicas.com/wp-content/uploads/2012/12/wallpaper-de-cachorro-15.jpg'
+    // }
+
 });
